ee008e93b4dfd79945e3c198eccedefc470072f6
abushahid81 <abushahidsardar@gmail.com>
2024-04-16 03:25:30 +0530

    csss

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// let togglebtn = document.getElementById("toggle-btn");
-// let body = document.body;
 
-// togglebtn.addEventListener("click", function(){
-//     body.classList.toggle("dark-mode");
-// })
 
 $(".form-check-input").click(function () {
-  $("button,h1, h2, h4, h5, p, a").toggleClass("change-text");
+  $("button,h1, h2, h4, h5, p, .nav-link").toggleClass("change-text");
   $("body").toggleClass("toggle-btn");
 });
