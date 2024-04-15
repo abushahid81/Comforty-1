b26bf787ac4eba96c8801e5290b10948e5750cd1
abushahid81 <abushahidsardar@gmail.com>
2024-04-16 03:43:15 +0530

    css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
 $(".form-check-input").click(function () {
-  $("button,h1, h2, h4, h5, p, .nav-link").toggleClass("change-text");
+  $("button,h1, h2, h4, h5, p, .nav-link, .bi-facebook, .bi-twitter, .bi-youtube, .bi-instagram,.bi-linkedin").toggleClass("change-text");
   $("body").toggleClass("toggle-btn");
 });
