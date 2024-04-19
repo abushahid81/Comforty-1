d7574913aed1e11e6ac3ca71b838211deefb0dd2
abushahid81 <abushahidsardar@gmail.com>
2024-04-19 20:17:53 +0530

    css

--- script.js
+++ script.js
@@ -4,3 +4,52 @@ $(".form-check-input").click(function () {
   $("button,h1, h2, h4, h5, p, .nav-link, .bi-facebook, .bi-twitter, .bi-youtube, .bi-instagram,.bi-linkedin").toggleClass("change-text");
   $("body").toggleClass("toggle-btn");
 });
+
+$('#slider-1').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true,
+        
+      },
+      600:{
+          items:4,
+          nav:true,
+      },
+      1000:{
+          items:4,
+          nav:true,
+      },
+      // 1400:{
+      //   item:5,
+      //   nav:true,
+      // }
+  }
+})
+$('#slider-2').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true,
+        
+      },
+      600:{
+          items:4,
+          nav:true,
+      },
+      1000:{
+          items:4,
+          nav:true,
+      },
+      // 1400:{
+      //   item:5,
+      //   nav:true,
+      // }
+  }
+})
